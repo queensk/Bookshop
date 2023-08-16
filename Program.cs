cd15ea06285a38a128b33799dacca5e1b0c6f2bc
queens kisivuli <queenskisivuli@gmail.com>
2023-08-16 14:51:53 +0300

    porject init

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection; // Add this line
+using BookShop.controller;
+using BookShop.Models.IService;
+using BookShop.services;
+using BookShop.Views;
+
+namespace BookShop
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddScoped<IBookShopService, BookService>()
+                .AddScoped<BookshopView>()
+                .AddScoped<BookshopController>()
+                .BuildServiceProvider();
+
+            var controller = serviceProvider.GetService<BookshopController>();
+
+            await controller.InitApplication();
+        }
+    }
+}
